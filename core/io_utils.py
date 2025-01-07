3e7ca3ac8fa6bc9048ec8b20be48122f7b720bbc
W2001-RF <rifaywassim@gmail.com>
2025-01-07 10:19:31 +0100

    add io utils

---
+++ core/io_utils.py
@@ -0,0 +1,12 @@
+from .models import City
+
+def read_cities(filename):
+    cities = []
+    with open(filename, "r") as file:
+        num_cities = int(file.readline().strip())
+        for _ in range(num_cities):
+            line = file.readline().strip().split()
+            ID = int(line[0])
+            x, y = map(int, line[1:])
+            cities.append(City(ID, x, y))
+    return cities
