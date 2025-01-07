15e1fb4b2e5412afe01ef0c3b2aa066a83f8056b
W2001-RF <rifaywassim@gmail.com>
2025-01-07 10:20:05 +0100

    add main file

---
+++ main.py
@@ -0,0 +1,21 @@
+from core.models import Route
+from core.io_utils import read_cities
+from core.algorithms import simulated_annealing
+from ui.visualizer import plot_progress
+import tkinter as tk
+
+def main():
+    cities = read_cities("berlin52.txt")
+    initial_route = Route(cities)
+    initial_route.calculate_distance()
+
+    root = tk.Tk()
+    canvas = tk.Canvas(root, width=750, height=500)
+    canvas.pack()
+    
+    best_route = simulated_annealing(initial_route)
+    print(f"Best distance: {best_route.distance}")
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
